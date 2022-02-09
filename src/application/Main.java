1929be5b57541b593e95505bf4a3775a6a48be05
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-02-09 15:55:05 +0100

    Resized window

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainSheet.fxml"));
-			Scene scene = new Scene(root,600,450);
+			Scene scene = new Scene(root,700,555);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
