1b360aa37b34c96c6836087c3b22b468f552f349
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-02-09 23:58:40 +0100

    new

--- src/com/gardhagen/joakim/kundStrukturSystemAB/person/seller/Seller.java
+++ src/com/gardhagen/joakim/kundStrukturSystemAB/person/seller/Seller.java
@@ -1,9 +1,13 @@
 package com.gardhagen.joakim.kundStrukturSystemAB.person.seller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gardhagen.joakim.kundStrukturSystemAB.person.Person;
+import com.gardhagen.joakim.kundStrukturSystemAB.person.customer.Customer;
 
 public class Seller extends Person {
-	
+	List <Customer> sellersCustomer = new ArrayList<>();
 	@Override
 	public String toString() {
 		return "[id=" + id + ", " + forName + ", " + lastName + ", " + Adress + "]";
