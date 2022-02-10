9b2e56264730ffc35379831e8ca711eea603b3cd
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-02-10 22:45:16 +0100

    nytt

--- src/application/MainSheetController.java
+++ src/application/MainSheetController.java
@@ -4,7 +4,6 @@ package application;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
